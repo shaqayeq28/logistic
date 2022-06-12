46a994cc66c1bff7da67fc6e5d41319c657cf56a
shaqayeq <you@example.com>
2022-06-12 16:27:16 +0430

    user

--- core/models.py
+++ core/models.py
@@ -1,2 +1,61 @@
+from django.contrib.auth.base_user import AbstractBaseUser
 from django.db import models
+from django.contrib.auth.models import  BaseUserManager, PermissionsMixin
+
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email,role, password=None, **extra_fields):
+        if not email:
+            raise ValueError('email must be provided')
+
+        user = self.model(
+            email=self.normalize_email(email),
+
+            **extra_fields
+        )
+        user.set_password(password)
+        user.role='admin'
+        user.save()
+        return user
+
+    def create_superuser(self, email, password):
+
+        if not email:
+            raise ValueError('email must be provided')
+
+        user = self.model(
+            email=self.normalize_email(email)
+
+        )
+        user.set_password(password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """Custom user model """
+
+    """
+        email: (username) , 
+        name  , is_active  , is_staff 
+    """
+    ROLE_CHOICES=(
+        ('admin', 'admin'),
+        ('reception', 'reception'),
+        ('technician', 'technician'),
+        ('inspector','inspector')
+    )
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=255)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    role=models.CharField(choices=ROLE_CHOICES,max_length=20)
+    objects = UserManager()
+    USERNAME_FIELD = 'email'
+
+    def __str__(self):
+        return self.name
 
